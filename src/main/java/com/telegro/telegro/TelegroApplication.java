b882d59a71e3bc5782b0e3cb2ee4de94b4782736
20210699 <20210699@duksung.ac.kr>
2024-09-10 04:28:03 +0900

    🎉 Init: 프로젝트 초기 세팅

---
+++ src/main/java/com/telegro/telegro/TelegroApplication.java
@@ -0,0 +1,13 @@
+package com.telegro.telegro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelegroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/telegro/telegro/TelegroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.telegro.telegro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
