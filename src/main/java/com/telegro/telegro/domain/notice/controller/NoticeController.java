1da4a7501ae1c64fc6ad4d4864cd846996a4968c
20210699 <20210699@duksung.ac.kr>
2024-09-13 01:17:13 +0900

    ✨ Feat: 프로젝트 초기 설정

---
+++ src/main/java/com/telegro/telegro/domain/notice/controller/NoticeController.java
@@ -0,0 +1,11 @@
+package com.telegro.telegro.domain.notice.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/notices")
+@RequiredArgsConstructor
+public class NoticeController implements NoticeControllerDocs {
+}
---
+++ src/main/java/com/telegro/telegro/domain/notice/controller/NoticeControllerDocs.java
@@ -0,0 +1,4 @@
+package com.telegro.telegro.domain.notice.controller;
+
+public interface NoticeControllerDocs {
+}
